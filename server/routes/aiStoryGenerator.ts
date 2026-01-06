d9124a5dfe3f2b8f8e8bbc4e79a3c1e15b1c54bb
wonseok_vivident <wslee@vivident.xyz>
2026-01-06 22:16:38 +0900

    try-catch 추가

--- server/routes/aiStoryGenerator.ts
+++ server/routes/aiStoryGenerator.ts
@@ -9,11 +9,26 @@ import path from "path";
 const router = express.Router();
 router.post("/story-generate", async (req, res) => {
     console.log("REQ BODY", req.body);
-    const { data, sheetName, sheetId, promptFile } = req.body;
-
-    const obj = parseSheetDataToObjects(data);
-    console.log(obj);
-
+    try {
+            const { data, dictionary, sheetName, sheetId, promptFile } = req.body;
+            if(
+                !data ||
+                !Array.isArray(data) ||
+                data.length === 0 ||
+                dictionary.length === 0 ||
+                sheetId === "" ||
+                sheetName === ""
+            ) {
+                return res.status(400).json({error : "Invalid data format"});
+            }
+    
+           
+            return res.status(200).json({ status: "OK"});
+    
+        } catch (err) {
+            console.error("Error in /ai/batch-group-translate", err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
 });
 
 export function parseSheetDataToObjects(data : any[][]) : Record<string, any>[] {
